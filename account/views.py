ce03f0f8844da0e7648345f23461fa235aa62a5e
NephygTdkingz <tdkingzict@gmail.com>
2025-07-18 21:39:56 +0100

    Step 2: Add user authentication and account app and user dashboard

---
+++ account/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('account:dashboard')
+    else:
+        form = UserCreationForm()
+    return render(request, 'authentication/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('account:dashboard')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'authentication/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('account:login')
+
+@login_required
+def dashboard_view(request):
+    return render(request, 'account/dashboard.html')
