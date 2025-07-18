583a9309c0d8c6112e58155b0fe18b52e5629d4e
NephygTdkingz <tdkingzict@gmail.com>
2025-07-18 23:47:46 +0100

    customized the register form and improved it

--- account/views.py
+++ account/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
-from .forms import CustomLoginForm
+from .forms import CustomLoginForm, CustomUserCreationForm
 
 def register_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('account:dashboard')
+        else:
+            # Add error message when authentication fails
+            messages.error(request, "Something went wrong, please check the errors and try again.")
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'authentication/register.html', {'form': form})
 
 def login_view(request):
