efb00e53c20da641febec9fe0a5e39f94423ac72
NephygTdkingz <tdkingzict@gmail.com>
2025-07-24 21:10:35 +0100

    started the process of intergrating posgres and sqlite fall back

--- invoice_generator/settings.py
+++ invoice_generator/settings.py
@@ -1,5 +1,13 @@
 import os
 from pathlib import Path
+from decouple import config, Csv
+import dj_database_url
+
+# Database
+# Load .env if it exists (for local dev)
+if os.path.exists('.env'):
+    import dotenv
+    dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,12 +17,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4i!+ih3w566)8_3ki4azrxs1a65bs^qm-8i0@ie(c+yz!-5y*6'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1', cast=Csv())
 
 
 # Application definition
@@ -65,11 +73,20 @@ WSGI_APPLICATION = 'invoice_generator.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL', default='sqlite:///db.sqlite3'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 LOGIN_URL = 'account:login'
